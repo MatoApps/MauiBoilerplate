b1a4d1242b6e6682bdc89d028f3758e992bc96f9
jevonsflash <jevonsflash@qq.com>
2022-05-19 20:56:24 +0800

    update

--- src/MauiBoilerplate/ViewModels/HomePageViewModel.cs
+++ src/MauiBoilerplate/ViewModels/HomePageViewModel.cs
@@ -1,9 +1,17 @@
-﻿using MauiBoilerplate.Core.ViewModel;
+﻿using Abp.Domain.Repositories;
+using MauiBoilerplate.Core.Entities;
+using MauiBoilerplate.Core.ViewModel;
 
 namespace MauiBoilerplate.ViewModels
 {
     public class HomePageViewModel : ViewModelBase
     {
+        private readonly IRepository<Song, long> songRepository;
+
+        public HomePageViewModel(IRepository<Song,long> songRepository)
+        {
+            this.songRepository=songRepository;
+        }
 
 
     }
